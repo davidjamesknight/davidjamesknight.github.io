0c6e52df43cd8e01d18b8b0a05509935a4367901
David Knight <djk@davids-mbp.mynetworksettings.com>
2025-05-15 17:51:40 -0700

    Restore current year in footer

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,7 +2,7 @@ const startYear = 2009;
 const currentYear = new Date().getFullYear();
 const numericString = (string) => string.replace(/\D/g, ""); /* Numbers only */
 
-// document.getElementById("current-year").textContent = currentYear;
+document.getElementById("current-year").textContent = currentYear;
 document.getElementById("years").textContent = currentYear - startYear;
 document.getElementById("mailto").addEventListener("click", () => {
   window.location.href = atob(
